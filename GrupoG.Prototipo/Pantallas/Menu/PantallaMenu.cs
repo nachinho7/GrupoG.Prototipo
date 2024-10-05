912240e48bf471592b792c8c54aaf96ff1e7ce43
nchp7 <nacho.porretta00@gmail.com>
2024-10-05 14:14:31 -0300

    edicion pantalla menu

--- GrupoG.Prototipo/Pantallas/Menu/PantallaMenu.cs
+++ GrupoG.Prototipo/Pantallas/Menu/PantallaMenu.cs
@@ -10,39 +10,39 @@ namespace GrupoG.Prototipo.Pantallas.Menu
             InitializeComponent();
         }
 
-        // Método que se llama cuando el botón 'Transportista' es clicado
         private void BotonTransportista_Click(object sender, EventArgs e)
         {
             PantallaTransportista form = new(); 
             form.ShowDialog();
         }
 
-        // Método que se llama cuando el botón 'Preparación' es clicado
         private void BotonPreparacion_Click(object sender, EventArgs e)
         {
             PantallaPreparacion form = new(); 
             form.ShowDialog();
         }
 
-        // Método que se llama cuando el botón 'Selección' es clicado
         private void BotonSeleccion_Click(object sender, EventArgs e)
         {
             PantallaSeleccion form = new();
             form.ShowDialog();
         }
 
-        // Método que se llama cuando el botón 'Entrega' es clicado
         private void BotonEntrega_Click(object sender, EventArgs e)
         {
             PantallaEntrega form = new();
             form.ShowDialog(); 
         }
 
-        // Método que se llama cuando el botón 'Despacho' es clicado
         private void BotonDespacho_Click(object sender, EventArgs e)
         {
             PantallaDespacho form = new(); 
             form.ShowDialog(); 
+        }
+
+        private void PantallaMenu_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
