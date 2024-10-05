876950bd09b5382f6ffe552526780ed5bf083519
nchp7 <nacho.porretta00@gmail.com>
2024-10-04 22:52:50 -0300

    Se agregaron los models a las pantallas

---
+++ GrupoG.Prototipo/Pantallas/Preparacion/PantallaPreparacionModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrupoG.Prototipo.Pantallas.Preparacion
+{
+    internal class PantallaPreparacionModel
+    {
+    }
+}
