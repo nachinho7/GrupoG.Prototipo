c3242511d944cc16a5c3ad93cc993ba1a8aa8244
STEPHANIE RAIZ <43SR38278150@campus.economicas.uba.ar>
2024-10-05 12:47:51 -0300

    Se crea lista de clientes en Pantalla Preparacion

--- GrupoG.Prototipo/Pantallas/Preparacion/PantallaPreparacionModel.cs
+++ GrupoG.Prototipo/Pantallas/Preparacion/PantallaPreparacionModel.cs
@@ -8,5 +8,21 @@ namespace GrupoG.Prototipo.Pantallas.Preparacion
 {
     internal class PantallaPreparacionModel
     {
-    }
+        public List<Clientes> Clientes { get; private set; } =
+        [
+            new Clientes { NumeroCliente = 2045 },
+            new Clientes { NumeroCliente = 3080 },
+            new Clientes { NumeroCliente = 1224 },
+            new Clientes { NumeroCliente = 8564 },
+            new Clientes { NumeroCliente = 8745 },
+        ];
+
+
+
+    };
+
+
+
+
+
 }
