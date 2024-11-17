5201e7e0e78c1c68332e9de484f0291b809e2c0c
nchp <nacho.porretta00@gmail.com>
2024-11-17 19:34:16 -0300

    cambios json

---
+++ GrupoG.Prototipo/Extra/Estados/PantallaConsultaEstados.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace GrupoG.Prototipo.Extra.Estados
+{
+    partial class PantallaConsultaEstados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            labelSeleccioneEstado = new Label();
+            comboBoxEstados = new ComboBox();
+            listViewConsultaEstados = new ListView();
+            numeroOrden = new ColumnHeader();
+            numeroCliente = new ColumnHeader();
+            groupBoxEstados = new GroupBox();
+            ClearEstados = new Button();
+            groupBoxOrden = new GroupBox();
+            ClearOrden = new Button();
+            textBoxEstadoOrden = new TextBox();
+            labelEstadoOrden = new Label();
+            labelOrden = new Label();
+            textBox1 = new TextBox();
+            BotonVolverMenu = new Button();
+            BuscarOrden = new Button();
+            groupBoxEstados.SuspendLayout();
+            groupBoxOrden.SuspendLayout();
+            SuspendLayout();
+            // 
+            // labelSeleccioneEstado
+            // 
+            labelSeleccioneEstado.AutoSize = true;
+            labelSeleccioneEstado.Location = new Point(19, 37);
+            labelSeleccioneEstado.Name = "labelSeleccioneEstado";
+            labelSeleccioneEstado.Size = new Size(101, 15);
+            labelSeleccioneEstado.TabIndex = 0;
+            labelSeleccioneEstado.Text = "Seleccione Estado";
+            // 
+            // comboBoxEstados
+            // 
+            comboBoxEstados.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBoxEstados.FormattingEnabled = true;
+            comboBoxEstados.Location = new Point(126, 34);
+            comboBoxEstados.Name = "comboBoxEstados";
+            comboBoxEstados.Size = new Size(121, 23);
+            comboBoxEstados.TabIndex = 1;
+            comboBoxEstados.SelectedIndexChanged += comboBoxEstados_SelectedIndexChanged;
+            // 
+            // listViewConsultaEstados
+            // 
+            listViewConsultaEstados.Columns.AddRange(new ColumnHeader[] { numeroOrden, numeroCliente });
+            listViewConsultaEstados.GridLines = true;
+            listViewConsultaEstados.Location = new Point(19, 63);
+            listViewConsultaEstados.Name = "listViewConsultaEstados";
+            listViewConsultaEstados.Size = new Size(307, 223);
+            listViewConsultaEstados.TabIndex = 2;
+            listViewConsultaEstados.UseCompatibleStateImageBehavior = false;
+            listViewConsultaEstados.View = View.Details;
+            // 
+            // numeroOrden
+            // 
+            numeroOrden.Text = "Nº Orden";
+            numeroOrden.Width = 150;
+            // 
+            // numeroCliente
+            // 
+            numeroCliente.Text = "Nº Cliente";
+            numeroCliente.Width = 150;
+            // 
+            // groupBoxEstados
+            // 
+            groupBoxEstados.Controls.Add(ClearEstados);
+            groupBoxEstados.Controls.Add(listViewConsultaEstados);
+            groupBoxEstados.Controls.Add(labelSeleccioneEstado);
+            groupBoxEstados.Controls.Add(comboBoxEstados);
+            groupBoxEstados.Location = new Point(12, 12);
+            groupBoxEstados.Name = "groupBoxEstados";
+            groupBoxEstados.Size = new Size(344, 326);
+            groupBoxEstados.TabIndex = 3;
+            groupBoxEstados.TabStop = false;
+            groupBoxEstados.Text = "Busque por Estados";
+            // 
+            // ClearEstados
+            // 
+            ClearEstados.Location = new Point(257, 292);
+            ClearEstados.Name = "ClearEstados";
+            ClearEstados.Size = new Size(69, 23);
+            ClearEstados.TabIndex = 3;
+            ClearEstados.Text = "Clear";
+            ClearEstados.UseVisualStyleBackColor = true;
+            ClearEstados.Click += ClearEstados_Click;
+            // 
+            // groupBoxOrden
+            // 
+            groupBoxOrden.Controls.Add(BuscarOrden);
+            groupBoxOrden.Controls.Add(ClearOrden);
+            groupBoxOrden.Controls.Add(textBoxEstadoOrden);
+            groupBoxOrden.Controls.Add(labelEstadoOrden);
+            groupBoxOrden.Controls.Add(labelOrden);
+            groupBoxOrden.Controls.Add(textBox1);
+            groupBoxOrden.Location = new Point(12, 355);
+            groupBoxOrden.Name = "groupBoxOrden";
+            groupBoxOrden.Size = new Size(344, 135);
+            groupBoxOrden.TabIndex = 4;
+            groupBoxOrden.TabStop = false;
+            groupBoxOrden.Text = "Busque por Orden";
+            // 
+            // ClearOrden
+            // 
+            ClearOrden.Location = new Point(243, 106);
+            ClearOrden.Name = "ClearOrden";
+            ClearOrden.Size = new Size(68, 23);
+            ClearOrden.TabIndex = 4;
+            ClearOrden.Text = "Clear";
+            ClearOrden.UseVisualStyleBackColor = true;
+            ClearOrden.Click += ClearOrden_Click;
+            // 
+            // textBoxEstadoOrden
+            // 
+            textBoxEstadoOrden.Location = new Point(165, 66);
+            textBoxEstadoOrden.Name = "textBoxEstadoOrden";
+            textBoxEstadoOrden.ReadOnly = true;
+            textBoxEstadoOrden.Size = new Size(146, 23);
+            textBoxEstadoOrden.TabIndex = 3;
+            // 
+            // labelEstadoOrden
+            // 
+            labelEstadoOrden.AutoSize = true;
+            labelEstadoOrden.Location = new Point(35, 69);
+            labelEstadoOrden.Name = "labelEstadoOrden";
+            labelEstadoOrden.Size = new Size(94, 15);
+            labelEstadoOrden.TabIndex = 2;
+            labelEstadoOrden.Text = "Estado de Orden";
+            // 
+            // labelOrden
+            // 
+            labelOrden.AutoSize = true;
+            labelOrden.Location = new Point(35, 34);
+            labelOrden.Name = "labelOrden";
+            labelOrden.Size = new Size(98, 15);
+            labelOrden.TabIndex = 1;
+            labelOrden.Text = "Ingrese Nº Orden";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(165, 31);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(146, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // BotonVolverMenu
+            // 
+            BotonVolverMenu.Location = new Point(220, 505);
+            BotonVolverMenu.Name = "BotonVolverMenu";
+            BotonVolverMenu.Size = new Size(136, 38);
+            BotonVolverMenu.TabIndex = 11;
+            BotonVolverMenu.Text = "Volver al Menú";
+            BotonVolverMenu.UseVisualStyleBackColor = true;
+            BotonVolverMenu.Click += BotonVolverMenu_Click;
+            // 
+            // BuscarOrden
+            // 
+            BuscarOrden.Location = new Point(165, 106);
+            BuscarOrden.Name = "BuscarOrden";
+            BuscarOrden.Size = new Size(68, 23);
+            BuscarOrden.TabIndex = 5;
+            BuscarOrden.Text = "Buscar";
+            BuscarOrden.UseVisualStyleBackColor = true;
+            BuscarOrden.Click += BuscarOrden_Click;
+            // 
+            // PantallaConsultaEstados
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(371, 555);
+            Controls.Add(BotonVolverMenu);
+            Controls.Add(groupBoxOrden);
+            Controls.Add(groupBoxEstados);
+            Name = "PantallaConsultaEstados";
+            Text = "Consulta Estados";
+            groupBoxEstados.ResumeLayout(false);
+            groupBoxEstados.PerformLayout();
+            groupBoxOrden.ResumeLayout(false);
+            groupBoxOrden.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label labelSeleccioneEstado;
+        private ComboBox comboBoxEstados;
+        private ListView listViewConsultaEstados;
+        private ColumnHeader numeroOrden;
+        private ColumnHeader numeroCliente;
+        private GroupBox groupBoxEstados;
+        private GroupBox groupBoxOrden;
+        private Label labelOrden;
+        private TextBox textBox1;
+        private TextBox textBoxEstadoOrden;
+        private Label labelEstadoOrden;
+        private Button ClearEstados;
+        private Button ClearOrden;
+        private Button BotonVolverMenu;
+        private Button BuscarOrden;
+    }
+}
---
+++ GrupoG.Prototipo/Extra/Estados/PantallaConsultaEstados.cs
@@ -0,0 +1,50 @@
+﻿using GrupoG.Prototipo.Menu;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrupoG.Prototipo.Extra.Estados
+{
+    public partial class PantallaConsultaEstados : Form
+    {
+        public PantallaConsultaEstados()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        public void ClearEstados_Click(object sender, EventArgs e)
+        {
+        }
+
+        public void ClearOrden_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void BotonVolverMenu_Click(object sender, EventArgs e)
+        {
+            PantallaMenu menu = new PantallaMenu();
+            this.Hide();
+            menu.StartPosition = FormStartPosition.CenterScreen;
+            menu.Location = this.Location;
+        }
+
+        private void comboBoxEstados_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BuscarOrden_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
